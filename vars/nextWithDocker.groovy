60b81a133cd031dbc8bf19e1cc00d724c2df16ac
PISETH Sattya <pisethsattya33@gmail.com>
2024-06-09 23:33:39 +0700

    Jenkins testing lib

--- vars/nextWithDocker.groovy
+++ vars/nextWithDocker.groovy
@@ -10,13 +10,13 @@ def call(String DOCKER_USER,String DOCKER_PASS,String TAGS, String IMAGE_NAME) {
         writeFile file: 'Dockerfile', text: dockerfileContent
 
         // Docker build
-        sh "docker build -t ${IMAGE_NAME}:${TAGS} ."
+        sh "docker build -t ${DOCKER_USER}/${IMAGE_NAME}:${TAGS} ."
 
         // Docker login (if required - uncomment and provide credentials)
         sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}"
 
         // Docker push (if required - uncomment)
-        sh "docker push ${IMAGE_NAME}:${TAGS}"
+        sh "docker push ${DOCKER_USER}/${IMAGE_NAME}:${TAGS}"
     } catch (Exception e) {
         echo "An error occurred: ${e.message}"
         currentBuild.result = 'FAILURE'
