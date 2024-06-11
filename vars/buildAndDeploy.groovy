42f428731965fe2106ce32e3bcf1fe996e24239a
PISETH Sattya <pisethsattya33@gmail.com>
2024-06-12 00:43:51 +0700

    testing

--- vars/buildAndDeploy.groovy
+++ vars/buildAndDeploy.groovy
@@ -1,11 +1,11 @@
 def call(Map config = [:]) {
+    // Load Dockerfile content from the library
+        def dockerfileContent = libraryResource('next.dockerfile')
+        writeFile file: 'Dockerfile', text: dockerfileContent
     // Default values
     def image = config.get('image', 'my-default-image')
     def registry = config.get('registry', 'my-default-registry')
     def tag = config.get('tag', 'latest')
     def containerPort = config.get('containerPort', '8080')
     def hostPort = config.get('hostPort', '8080')
-
-    def dockerfileContent = libraryResource 'angular.dockerfile'
-    writeFile file: 'Dockerfile', text: dockerfileContent
 }
